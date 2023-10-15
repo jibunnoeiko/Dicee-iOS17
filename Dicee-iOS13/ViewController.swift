d9c6ede537691ff1734edfbc8ec538158d2b6343
jibunnoeiko <geralt_beliy_wolk@outlook.com>
2023-10-15 17:27:10 +0200

    Обновил логику игры, добавил вариативность

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -6,15 +6,37 @@
 //  Copyright © 2019 London App Brewery. All rights reserved.
 //
 
+// Импортируем UIKit, который предоставляет основные классы и функции для разработки пользовательских интерфейсов в iOS.
 import UIKit
 
+// Определение класса ViewController, который наследуется от UIViewController.
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    // Связываем элементы интерфейса с кодом, используя @IBOutlet.
+    @IBOutlet weak var diceViewImage1: UIImageView!
+    @IBOutlet weak var diceViewImage2: UIImageView!
+
+    // Обработчик нажатия на кнопку.
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        // Создаем массив с изображениями для шестигранных костей.
+        let diceArray = [
+            UIImage(imageLiteralResourceName: "DiceOne"),
+            UIImage(imageLiteralResourceName: "DiceTwo"),
+            UIImage(imageLiteralResourceName: "DiceThree"),
+            UIImage(imageLiteralResourceName: "DiceFour"),
+            UIImage(imageLiteralResourceName: "DiceFive"),
+            UIImage(imageLiteralResourceName: "DiceSix")
+        ]
+        
+        // Устанавливаем случайное изображение для первой кости из массива.
+        diceViewImage1.image = diceArray.randomElement()
+        
+        // Устанавливаем случайное изображение для второй кости из массива.
+        diceViewImage2.image = diceArray.randomElement()
+        
     }
-
-
+    
 }
+
 
